1f0932179af0723ef5024d05bed2f5c3bcc65714
MewadaChirag <mewada.chirag@gmail.com>
2022-07-20 18:31:08 +0530

    Bootstrap

---
+++ MvcMovies/Controllers/TestController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MvcMovies.Controllers
+{
+    public class TestController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
