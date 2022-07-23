7bd044c4978c5f655626b806c3749b8bf9e9cd2a
MewadaChirag <mewada.chirag@gmail.com>
2022-07-23 11:58:22 +0530

    Book controller and views

---
+++ MvcMovies/Controllers/BookController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcMovies.Models;
+using MvcMovies.Repositories;
+
+namespace MvcMovies.Controllers
+{
+    public class BookController : Controller
+    {
+        private readonly BookRepository _bookrepository;
+        public BookController()
+        {
+            _bookrepository = new BookRepository();
+        }
+        public ViewResult GetAllBooks()
+        {
+            List<BookModel> booksdata = _bookrepository.GetAllBooks();
+            return View(booksdata);
+        }
+        public ViewResult GetBookById(int id)
+        {
+            BookModel bookdata = _bookrepository.GetBookById(id);
+            return View(bookdata);
+        }
+        public ViewResult SearchBook(string title, string authorname)
+        {
+            List<BookModel> booksdata = _bookrepository.SearchBook(title, authorname);
+            return View(booksdata);
+                
+        }
+    }
+}
---
+++ MvcMovies/Repositories/BookRepository.cs
@@ -0,0 +1,31 @@
+﻿using MvcMovies.Models;
+using System.Collections.Generic;
+
+namespace MvcMovies.Repositories
+{
+    public class BookRepository
+    {
+        private List<BookModel> DataSource()
+        {
+            return new List<BookModel>()
+            {
+                new BookModel(){Id=1, Title="MVC", Author="Chirag", Description="This book is all about MVC. It covers all concepts."},
+                new BookModel(){Id=2, Title="WebAPI", Author="Nitish", Description="This book is all about WebAPI. It covers all concepts."},
+                new BookModel(){Id=3, Title="Angular", Author="Priynka", Description="This book is all about Angular. It covers all concepts."},
+                new BookModel(){Id=4, Title="CSS", Author="Nargis", Description="This book is all about CSS. It covers all concepts."}
+            };
+        }
+        public List<BookModel> GetAllBooks()
+        {
+            return DataSource();
+        }
+        public BookModel GetBookById(int id)
+        {
+            return DataSource().FirstOrDefault(x => x.Id == id);
+        }
+        public List<BookModel> SearchBook(string title, string authorname)
+        {
+            return DataSource().FindAll(x => x.Title.Contains(title) || x.Author.Contains(authorname));
+        }
+    }
+}
